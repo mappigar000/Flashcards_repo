525c9ff2c6198e7ad937cbe46851bf59f32cb5a5
mappigar000 <gjm2002.2021@gmail.com>
2025-11-03 10:56:46 -0600

    added edit screen
    Added functional edit screen where one can add and delete cards. Also cleaned up the root window.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 from tkinter import *
 from tkinter import ttk
-from utils.helpers import add_card, save_settings, load_settings
+from utils.helpers import card_screen, practice_sequence, settings_screen, save_settings, load_settings, window_sizer, make_grid_expandable
 
 def main():
     boot_settings = load_settings()
@@ -15,25 +15,18 @@ def main():
 
     root = Tk()
     root.title("FlashBaby")
+    root.geometry(window_sizer(root, 0.6, 0.5))
+    make_grid_expandable(root)
+
     frm = ttk.Frame(root, padding=100)
-    frm.grid()
-    ttk.Button(frm, text="add card", command=lambda: add_card(root, flashcards)).grid(column=0, row=0)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+    make_grid_expandable(frm, rows=4, cols=1)
+    frm.grid(sticky="nsew")
+
+    ttk.Button(frm, width=100, text="Practice", command=lambda: practice_sequence()).grid(column=0, row=0)
+    ttk.Button(frm, width=100, text="See Cards", command=lambda: card_screen(root, flashcards)).grid(column=0, row=1)
+    ttk.Button(frm, width=100, text="Settings", command=lambda: settings_screen()).grid(column=0, row=2)
+    ttk.Button(frm, width=100, text="Quit", command=root.destroy).grid(column=0, row=3)
     root.mainloop()
-'''
-    while True:
-        user_input = input("Enter a command\n")
 
-        if user_input == "add":
-            add_card(flashcards)
-
-        elif user_input == "print":
-            with open(settings['flashcardpack'], "r") as f:
-                flashcards = json.load(f)
-            print(flashcards)
-
-        elif user_input == "quit":
-            break
-'''
 if __name__ == "__main__":
     main()
